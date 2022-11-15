ee469c447e48fb096be98a910d5c8466f4c71a37
Ahmed1Dev <100456777+Ahmed1Dev@users.noreply.github.com>
2022-11-15 21:40:07 +0200

    Fix Add Image Option On Mongoose

--- examples/custom-databases/mongoose.js
+++ examples/custom-databases/mongoose.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({
-    intents: [Discord.IntentsBitField.Flags.Guilds, Discord.IntentsBitField.Flags.GuildMessageReactions]
+    intents: [
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+    ]
 });
 
 // Connect to the database
@@ -15,65 +18,61 @@ db.once('open', () => {
 });
 
 // Create the schema for giveaways
-const giveawaySchema = new mongoose.Schema(
-    {
-        messageId: String,
-        channelId: String,
-        guildId: String,
-        startAt: Number,
-        endAt: Number,
-        ended: Boolean,
-        winnerCount: Number,
-        prize: String,
-        messages: {
-            giveaway: String,
-            giveawayEnded: String,
-            title: String,
-            inviteToParticipate: String,
-            drawing: String,
-            dropMessage: String,
-            winMessage: mongoose.Mixed,
-            embedFooter: mongoose.Mixed,
-            noWinner: String,
-            winners: String,
-            endedAt: String,
-            hostedBy: String
-        },
-        thumbnail: String,
-        image: String,
-        hostedBy: String,
-        winnerIds: { type: [String], default: undefined },
-        reaction: mongoose.Mixed,
-        botsCanWin: Boolean,
-        embedColor: mongoose.Mixed,
-        embedColorEnd: mongoose.Mixed,
-        exemptPermissions: { type: [], default: undefined },
-        exemptMembers: String,
-        bonusEntries: String,
-        extraData: mongoose.Mixed,
-        lastChance: {
-            enabled: Boolean,
-            content: String,
-            threshold: Number,
-            embedColor: mongoose.Mixed
-        },
-        pauseOptions: {
-            isPaused: Boolean,
-            content: String,
-            unPauseAfter: Number,
-            embedColor: mongoose.Mixed,
-            durationAfterPause: Number,
-            infiniteDurationText: String
-        },
-        isDrop: Boolean,
-        allowedMentions: {
-            parse: { type: [String], default: undefined },
-            users: { type: [String], default: undefined },
-            roles: { type: [String], default: undefined }
-        }
+const giveawaySchema = new mongoose.Schema({
+    messageId: String,
+    channelId: String,
+    guildId: String,
+    startAt: Number,
+    endAt: Number,
+    ended: Boolean,
+    winnerCount: Number,
+    prize: String,
+    messages: {
+        giveaway: String,
+        giveawayEnded: String,
+        inviteToParticipate: String,
+        drawing: String,
+        dropMessage: String,
+        winMessage: mongoose.Mixed,
+        embedFooter: mongoose.Mixed,
+        noWinner: String,
+        winners: String,
+        endedAt: String,
+        hostedBy: String
     },
-    { id: false }
-);
+    thumbnail: String,
+    image: String,
+    hostedBy: String,
+    winnerIds: { type: [String], default: undefined },
+    reaction: mongoose.Mixed,
+    botsCanWin: Boolean,
+    embedColor: mongoose.Mixed,
+    embedColorEnd: mongoose.Mixed,
+    exemptPermissions: { type: [], default: undefined },
+    exemptMembers: String,
+    bonusEntries: String,
+    extraData: mongoose.Mixed,
+    lastChance: {
+        enabled: Boolean,
+        content: String,
+        threshold: Number,
+        embedColor: mongoose.Mixed
+    },
+    pauseOptions: {
+        isPaused: Boolean,
+        content: String,
+        unPauseAfter: Number,
+        embedColor: mongoose.Mixed,
+        durationAfterPause: Number,
+        infiniteDurationText: String
+    },
+    isDrop: Boolean,
+    allowedMentions: {
+        parse: { type: [String], default: undefined },
+        users: { type: [String], default: undefined },
+        roles: { type: [String], default: undefined }
+    }
+}, { id: false });
 
 // Create the model
 const giveawayModel = mongoose.model('giveaways', giveawaySchema);
@@ -97,7 +96,7 @@ const GiveawayManagerWithOwnDatabase = class extends GiveawaysManager {
     // This function is called when a giveaway needs to be edited in the database.
     async editGiveaway(messageId, giveawayData) {
         // Find by messageId and update it
-        await giveawayModel.updateOne({ messageId }, giveawayData).exec();
+        await giveawayModel.updateOne({ messageId }, giveawayData, { omitUndefined: true }).exec();
         // Don't forget to return something!
         return true;
     }
